640abea2f1c71d4811a9c83a3dd72f9bdf6e3201
cabustillo13 <cabustillo13@hotmail.com>
2020-07-05 03:17:29 -0300

    Probando nuevas cosas

---
+++ Preprocesamiento.py
@@ -0,0 +1,77 @@
+##PARA CORTAR UNA SOLA IMAGEN
+import cv2
+import numpy as np
+
+#Path absoluto
+path="/home/carlos/Documentos/Otros/Resolvedor-de-Sudoku/Screenshots/"
+#Imagen a analizar
+imagen = cv2.imread(path+"photo0.png")
+
+#######################################################################
+################    Funcion para rotar imagen               ###########
+#######################################################################
+def rotarImagen(image, angle):
+     image_center = tuple(np.array(image.shape[1::-1]) / 2)
+     rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+     result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+     return result
+
+#######################################################################
+################    Recortar franja de arriba imagen        ###########
+#######################################################################
+def recortar(imagen,x):
+    #x determina hasta donde cortar
+    #fileb determina con que nombre lo vamos a guardar a la imagen
+    #Determinar dimensiones de la imagen
+    height, width, channels = imagen.shape
+
+    crop_img = imagen[x:height, 0:width]
+    return crop_img
+
+#######################################################################
+################    Recortar cuadradito                     ###########
+#######################################################################
+def recortarCuadrado(imagen,x,y,h,w):
+    
+    #Cada lado del cuadrado/casilla tiene un lado de longitud 10
+    crop_img = imagen[x:(x+h), y:(y+w)]
+    return crop_img
+
+#######################################################################
+################                Guardar imagen              ###########
+#######################################################################
+def guardar(imagen,fileb):
+    
+    new_path = "/home/carlos/Documentos/Otros/Sudoku/Imagenes/"
+    cv2.imwrite(new_path + fileb, imagen)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+#######################################################################
+################                    El main                 ###########
+#######################################################################
+
+if __name__ == '__main__':    
+    
+    ##PREPROCESAMIENTO -> Aca recordamos los bordes, publicidad y todo las imagenes ajenas al tablero del sudoku
+    imagen = recortar(imagen,218)
+    imagen = rotarImagen(imagen,180)
+    imagen = recortar(imagen,348)
+    imagen = rotarImagen(imagen,180)
+    
+    ##RECORTAR CADA CUADRADITO DE FORMA INDIVIDUAL
+    contador=0
+    w=0
+    for j in range(9):
+        
+        h=0
+        for i in range(9):
+            #A prueba y error determine los valores de los lados de los cuadrados
+            nombre = "prueba"+ str(contador) + ".png"
+            imagen1 = recortarCuadrado(imagen,0,h,75,80)
+            guardar(imagen1,nombre)
+            h=h+80
+            contador=contador+1
+        w=w+75
+        
+    
---
+++ Resolvedor.py
@@ -0,0 +1,138 @@
+#Resolvedor de SUDOKU 3X3
+#Modificado por: Carlos Bustillo
+#Original:https://github.com/jorditorresBCN/Sudoku/blob/master/Sudoku.ipynb
+
+rows = 'ABCDEFGHI'
+cols = '123456789'
+
+def cross(A, B):
+    "Cross product of elements in A and elements in B."
+    return [a+b for a in A for b in B]
+
+boxes = cross(rows, cols)
+row_units = [cross(r, cols) for r in rows]
+column_units = [cross(rows, c) for c in cols]
+square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+
+unitlist = row_units + column_units + square_units
+
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+
+#Para mostrar la grilla
+def display(values):  
+    width = 1+max(len(values[s]) for s in boxes)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF': print(line)
+    return
+
+def grid_values_original(grid):
+    return dict(zip(boxes, grid))
+
+def grid_values(grid):
+    values = []
+    for c in grid:
+        if c == '.':
+            values.append('123456789')
+        elif c in '123456789':
+            values.append(c)
+    return dict(zip(boxes, values))
+
+#Elimina los valores que ya estan dentro de la grilla
+def eliminate(values):
+    """Eliminate values from peers of each box with a single value.
+    """
+    
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            values[peer] = values[peer].replace(digit,'')
+    return values
+
+def only_choice(values):
+    for unit in unitlist:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                values[dplaces[0]] = digit
+    return values
+
+def reduce_sudoku(values):
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+
+        # se the Eliminate Strategy
+        values = eliminate(values)
+
+        # Use the Only Choice Strategy
+        values = only_choice(values)
+
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
+def search(values):
+    values = reduce_sudoku(values)
+    if values is False:
+        return False ## Failed earlier
+    if all(len(values[s]) == 1 for s in boxes): 
+        return values ## Solved!
+    
+    # Choose one of the unfilled squares with the fewest possibilities
+    unfilled_squares= [(len(values[s]), s) for s in boxes if len(values[s]) > 1]
+    n,s = min(unfilled_squares)
+    
+    # recurrence to solve each one of the resulting sudokus
+    for value in values[s]:
+        nova_sudoku = values.copy()
+        nova_sudoku[s] = value
+        attempt = search(nova_sudoku)
+        if attempt:
+            return attempt
+def solve(grid):
+    # Create a dictionary of values from the grid
+    values = grid_values(grid)
+    return search(values)
+
+#El main
+if __name__ == '__main__':
+    
+    #Aca ingreso la disposicion a resolver
+    #sudoku_grid = '..5..96.......3.2.97......8.1...8...4...5...7...6...9.5......89.4.2.......25..1..'
+    
+    #Ingreso de fila individual
+    linea1='.4./.../79.'
+    linea2='9.6/..8/...'
+    linea3='2../4../.56'
+    linea4='164/.../9..'
+    linea5='.../135/...'
+    linea6='..8/.../271'
+    linea7='67./..4/..9'
+    linea8='.../6../1.7'
+    linea9='.81/.../.2.'
+    lineas = linea1 + linea2 + linea3 + linea4 + linea5 + linea6 + linea7 + linea8 + linea9
+    lineas=lineas.translate(None,'/')
+    
+    #print(sudoku_grid)
+    #print(lineas)
+    
+    print ("original:")
+    #display(grid_values_original(sudoku_grid))
+    display(grid_values_original(lineas))
+    print (" ")
+    print ("solucion:")
+    #display(solve(sudoku_grid))
+    display(solve(lineas))
+
+
+
